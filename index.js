17b9004a51243354f21dddb7311542570fb99fc9
CristianFletcher <86000995+CristianFletcher@users.noreply.github.com>
2021-06-17 12:25:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import React from "react"
 export default function Home() {
   return <div>Hello world!</div>
 }
+add_img() { 
+	var img = document.createElement('img'); 
+    img.src = ‘C:\Users\Andrei\Desktop’; 
+	document.getElementById('body').appendChild(img);
