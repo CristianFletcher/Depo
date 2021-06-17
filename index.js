ceb0e7679d5c6260f2c2c1aaf89769cff2997c60
CristianFletcher <86000995+CristianFletcher@users.noreply.github.com>
2021-06-17 13:00:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Test modificare titlu</div>
-}
-add_img() { 
-	var img = document.createElement('img'); 
-    img.src = ‘C:\Users\Andrei\Desktop’; 
-	document.getElementById('body').appendChild(img);
-	
-gatsby new
 
