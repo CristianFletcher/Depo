20893b395e9b9afcb8b5435067cd7d67eb660bc1
CristianFletcher <86000995+CristianFletcher@users.noreply.github.com>
2021-06-17 13:11:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import React from "react"
-cd my-hello-world-starter/
-gatsby develop
-
+gatsby new
+What would you like to call your site?
+✔ · My First Gatsby Site
+What would you like to name the folder where your site will be created?
+✔ Desktop/ my-first-gatsby-site
+✔ Will you be using a CMS?
+· No (or I'll add it later)
+✔ Would you like to install a styling system?
+· No (or I'll add it later)
